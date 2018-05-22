c1151d609672407f2203c23672490d157e1e4a63
Preston Smith <preston.s1993@gmail.com>
2018-05-22 14:32:29 -0500

    Update version

--- src/Client/Settings.java
+++ src/Client/Settings.java
@@ -46,7 +46,7 @@ public class Settings {
 	// Internally used variables
 	public static boolean fovUpdateRequired;
 	public static boolean versionCheckRequired = true;
-	public static final double VERSION_NUMBER = 20180522.075737;
+	public static final double VERSION_NUMBER = 20180522.193212;
 	/**
 	 * A time stamp corresponding to the current version of this source code. Used as a sophisticated versioning system.
 	 *
