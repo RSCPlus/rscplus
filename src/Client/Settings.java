8cfd86f399ad8d127e39419d2390892b86731c91
nickzuber <nickzoober@gmail.com>
2018-05-22 18:11:35 -0400

    Update version

--- src/Client/Settings.java
+++ src/Client/Settings.java
@@ -46,7 +46,7 @@ public class Settings {
 	// Internally used variables
 	public static boolean fovUpdateRequired;
 	public static boolean versionCheckRequired = true;
-	public static final double VERSION_NUMBER = 20180522.200651;
+	public static final double VERSION_NUMBER = 20180522.221121;
 	/**
 	 * A time stamp corresponding to the current version of this source code. Used as a sophisticated versioning system.
 	 *
