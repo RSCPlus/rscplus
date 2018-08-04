e46ac8caedcf9f2ae095a1b00479da1182d7d493
Preston Smith <preston.s1993@gmail.com>
2018-08-04 18:53:34 -0500

    rscplus: Fix first time config generation

--- src/Client/Settings.java
+++ src/Client/Settings.java
@@ -45,7 +45,7 @@ public class Settings {
   public static boolean fovUpdateRequired;
   public static boolean versionCheckRequired = true;
   public static int javaVersion = 0;
-  public static final double VERSION_NUMBER = 20180804.233318;
+  public static final double VERSION_NUMBER = 20180804.235337;
   /**
    * A time stamp corresponding to the current version of this source code. Used as a sophisticated
    * versioning system.
@@ -1096,7 +1096,7 @@ public class Settings {
       if (!configFile.isDirectory()) {
         if (!configFile.exists()) {
           definePresets(props);
-          save("default");
+          save("custom");
         }
       }
 
