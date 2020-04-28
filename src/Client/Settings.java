22d9d4dd9585679bbd98b6f0d9692602028caac6
rscbuddy <dance2gf@gmail.com>
2020-04-28 10:26:53 -0500

    Automated version update and source format

--- src/Client/Settings.java
+++ src/Client/Settings.java
@@ -42,7 +42,7 @@ public class Settings {
   public static boolean fovUpdateRequired;
   public static boolean versionCheckRequired = true;
   public static int javaVersion = 0;
-  public static final double VERSION_NUMBER = 20200422.185218;
+  public static final double VERSION_NUMBER = 20200428.152626;
   /**
    * A time stamp corresponding to the current version of this source code. Used as a sophisticated
    * versioning system.
