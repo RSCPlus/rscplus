c707cf00ebb97d81ce2cfa367d482a9bba091e46
Logg <walnut.alligator@gmail.com>
2023-06-13 16:32:08 -0500

    Release v20230614.060900

--- src/Client/Settings.java
+++ src/Client/Settings.java
@@ -52,7 +52,7 @@ public class Settings {
   public static boolean renderingScalarUpdateRequired;
   public static boolean versionCheckRequired = true;
   public static int javaVersion = 0;
-  public static final double VERSION_NUMBER = 20230505.050505;
+  public static final double VERSION_NUMBER = 20230614.060900;
   public static boolean successfullyInitted = false;
   /**
    * A time stamp corresponding to the current version of this source code. Used as a sophisticated
